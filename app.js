99586de1a8131216dc5573504d9c7d4cc60ae383
niklasHagner <niklas.hagner@outlook.com>
2020-03-31 02:32:52 +0200

    Use unix file endings

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = 3000;
 const { scrapeUrl } = require('./scrape.js');
 const cors = require('cors')
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-// //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
 
 app.get('/test', async function (req, res) {
     res.json({pizza:true});
--- scrape.js
+++ scrape.js
@@ -1,6 +1,4 @@
 const puppeteer = require('puppeteer');
-const fs = require('fs');
-const moment = require("moment");
 
 async function scrapeUrl(url = "https://www.metacritic.com/movie/shutter-island/critic-reviews") {
   const browser = await puppeteer.launch();
@@ -9,28 +7,6 @@ async function scrapeUrl(url = "https://www.metacritic.com/movie/shutter-island/
   await page.goto(url, { waitUntil: 'networkidle0' });
 
   let html = await page.evaluate(() => {
-    // const divisionElements = [...document.querySelectorAll(".weight-rankings")];
-    // const divisions = divisionElements.map((division) => { 
-    //   return { 
-    //     name: division.querySelector("h3").innerText, 
-    //     fighters:  [...division.querySelectorAll(".fighter")].map((fighter) => {
-    //       return {
-    //         rank: fighter.querySelector(".rank").innerText,
-    //         link: "https://mma-stats.com" + fighter.querySelector("a").getAttribute("href"),
-    //         name: fighter.querySelector("a").innerText
-    //       };
-    //     })
-    //   }
-    // });
-
-    // const selectedDate = document.querySelector("#date_select").selectedOptions[0].innerText;
-
-    // const json = {
-    //   date: selectedDate,
-    //   divisions
-    // }
-    // return json;
-
     return document.documentElement.innerHTML;
   });
 
